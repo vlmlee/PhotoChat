d6f59d2e2193c35741f19dab18e278eac0f7aca1
Michael Lee <mlee@vellichorlabs.com>
2016-09-11 19:59:03 -0800

    Created project, set up file structure, set up server, created index.html, 404.html, and style.css. Influenced greatly by the example chat app on socket.io's main website.

---
+++ server/server.js
@@ -0,0 +1,58 @@
+const http = require('http'),
+    fs = require('fs'),
+    path = require('path'),
+    mime = require('mime');
+
+var cache = {};
+
+// Routes root url to '/public/index.html' and sends 404 error to all HTTP requests that have no endpoint.
+
+var staticServer = {
+    send404: function(response) {
+        fs.readFile('./public/404.html', 'utf-8', (err, content) => {
+        	response.writeHead(404, { 'Content-Type': 'text/html' });
+        	response.write(content);
+        	response.end();
+        });
+    },
+    sendFile: function(response, filePath, fileContents) {
+        response.writeHead(200, { "content-type": mime.lookup(path.basename(filePath)) });
+        response.end(fileContents);
+    },
+    sendStatic: function(response, cache, absPath) {
+        if (cache[absPath]) {
+            this.sendFile(response, absPath, cache[absPath]);
+        } else {
+            fs.exists(absPath, (exists) => {
+                if (exists) {
+                    fs.readFile(absPath, (err, data) => {
+                        if (err) this.send404(response);
+
+                        cache[absPath] = data;
+                        this.sendFile(response, absPath, data);
+                    });
+                } else {
+                    this.send404(response);
+                }
+            });
+        }
+    }
+}
+
+// Main HTTP server instance
+var server = http.createServer((request, response) => {
+    var filePath;
+
+    if (request.url == '/') {
+        filePath = 'public/index.html';
+    } else {
+        filePath = 'public' + request.url;
+    }
+
+    var absPath = './' + filePath;
+    staticServer.sendStatic(response, cache, absPath);
+});
+
+server.listen(3000, () => {
+    console.log('Listening...');
+});
